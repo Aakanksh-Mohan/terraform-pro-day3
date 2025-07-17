15636cb8446b27af282ad66b4ea2b8ad63cc7d68
Aakanksh-Mohan <aakanksh2510@gmail.com>
2025-07-17 04:47:50 +0000

    initial commit

--- resource.tf
+++ resource.tf
@@ -1,6 +1,10 @@
-resource "azurerm_resource_group" "terra-day3" {
-  name     = "terra"
+resource "azurerm_resource_group" "rg" {
+  name     = "terra1-day3"
   location = "East US"
 }
  
+resource "azurerm_resource_group" "rg" {
+  name     = "terra2-day3"
+  location = "East US"
+}
  
